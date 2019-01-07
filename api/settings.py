516b752686e7c8239834fddaa20f625293ae2964
gauravpandey <gaurav.pandey1234546@gmail.com>
2019-01-08 00:03:45 +0530

    Fixed

--- api/settings.py
+++ api/settings.py
@@ -47,6 +47,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         #'rest_framework.permissions.AllowAny',
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
